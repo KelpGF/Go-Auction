5382d514492e678606f3535495a0d9eae725fc2d
KelpGF <kelpgf@gmail.com>
2024-11-25 18:28:37 -0300

    feat: add mongodb configs

--- cmd/auction/main.go
+++ cmd/auction/main.go
@@ -1,14 +1,29 @@
 package main
 
 import (
+	"context"
 	"log"
 
+	"github.com/KelpGF/Go-Auction/config/database/mongodb"
+	"github.com/KelpGF/Go-Auction/config/logger"
 	"github.com/joho/godotenv"
 )
 
 func main() {
-	if err := godotenv.Load(); err != nil {
+	logger.Info("Starting the application...")
+
+	ctx := context.Background()
+
+	if err := godotenv.Load("cmd/auction/.env"); err != nil {
 		log.Fatal("Error loading .env file: ", err)
 		return
 	}
+
+	_, err := mongodb.NewMongoDBConnection(ctx)
+	if err != nil {
+		log.Fatal("Error connecting to MongoDB: ", err)
+		return
+	}
+
+	logger.Info("MongoDB connection successful")
 }
---
+++ config/database/mongodb/connection.go
@@ -0,0 +1,37 @@
+package mongodb
+
+import (
+	"context"
+	"os"
+
+	"github.com/KelpGF/Go-Auction/config/logger"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+const (
+	MONGODB_URL    = "MONGODB_URL"
+	MONGODB_DBNAME = "MONGODB_DB"
+)
+
+func NewMongoDBConnection(ctx context.Context) (*mongo.Database, error) {
+	mongoURL := os.Getenv(MONGODB_URL)
+	mongoDBName := os.Getenv(MONGODB_DBNAME)
+
+	client, err := mongo.Connect(
+		ctx,
+		options.Client().ApplyURI(mongoURL),
+	)
+
+	if err != nil {
+		logger.Error("Error connecting to MongoDB", err)
+		return nil, err
+	}
+
+	if err := client.Ping(ctx, nil); err != nil {
+		logger.Error("Error pinging to MongoDB", err)
+		return nil, err
+	}
+
+	return client.Database(mongoDBName), nil
+}
