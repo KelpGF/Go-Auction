a2d6e3d1d67129504ac8c0e5c6207c04697265af
KelpGF <kelpgf@gmail.com>
2024-11-26 19:20:23 -0300

    feat: add find user usecase

--- internal/entity/user_entity/user_entity.go
+++ internal/entity/user_entity/user_entity.go
@@ -1,6 +1,16 @@
 package user_entity
 
+import (
+	"context"
+
+	"github.com/KelpGF/Go-Auction/internal/internal_error"
+)
+
 type User struct {
 	ID   string
 	Name string
+}
+
+type UserRepositoryInterface interface {
+	FindUserByID(ctx context.Context, id string) (*User, *internal_error.InternalError)
 }
---
+++ internal/usecase/user_usecase/find_user_usecase.go
@@ -0,0 +1,33 @@
+package user_usecase
+
+import (
+	"context"
+
+	"github.com/KelpGF/Go-Auction/internal/entity/user_entity"
+	"github.com/KelpGF/Go-Auction/internal/internal_error"
+)
+
+type UserOutputDTO struct {
+	ID   string
+	Name string
+}
+
+type UserUseCaseInterface interface {
+	FindUserById(ctx context.Context, id string) (*UserOutputDTO, *internal_error.InternalError)
+}
+
+type UserUseCase struct {
+	UserRepository user_entity.UserRepositoryInterface
+}
+
+func (u *UserUseCase) FindUserById(ctx context.Context, id string) (*UserOutputDTO, *internal_error.InternalError) {
+	user, err := u.UserRepository.FindUserByID(ctx, id)
+	if err != nil {
+		return nil, err
+	}
+
+	return &UserOutputDTO{
+		ID:   user.ID,
+		Name: user.Name,
+	}, nil
+}
