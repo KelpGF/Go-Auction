2129be0c6beb2e224ab777ddc9cfd3504a94a5a6
KelpGF <kelpgf@gmail.com>
2024-11-27 19:58:10 -0300

    feat: add bid entity factory

--- internal/entity/bid_entity/bid_entity.go
+++ internal/entity/bid_entity/bid_entity.go
@@ -5,6 +5,7 @@ import (
 	"time"
 
 	"github.com/KelpGF/Go-Auction/internal/internal_error"
+	"github.com/google/uuid"
 )
 
 type Bid struct {
@@ -13,6 +14,42 @@ type Bid struct {
 	AuctionID string
 	Amount    float64
 	Timestamp time.Time
+}
+
+func NewBid(
+	userID, auctionID string,
+	amount float64,
+) (*Bid, *internal_error.InternalError) {
+	bid := &Bid{
+		ID:        uuid.New().String(),
+		UserID:    userID,
+		AuctionID: auctionID,
+		Amount:    amount,
+		Timestamp: time.Now(),
+	}
+
+	err := bid.Validate()
+	if err != nil {
+		return nil, err
+	}
+
+	return bid, nil
+}
+
+func (b *Bid) Validate() *internal_error.InternalError {
+	if err := uuid.Validate(b.UserID); err != nil {
+		return internal_error.NewBadRequestError("UserId id is not a valid ID")
+	}
+
+	if err := uuid.Validate(b.AuctionID); err != nil {
+		return internal_error.NewBadRequestError("AuctionID id is not a valid ID")
+	}
+
+	if b.Amount <= 0 {
+		return internal_error.NewBadRequestError("Amount must be greater than 0")
+	}
+
+	return nil
 }
 
 type BidRepositoryInterface interface {
